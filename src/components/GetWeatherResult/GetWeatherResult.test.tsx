ba9a732b373732884931a26f293b2bcbd17f3146
RyanRJ <ryanrj@hotmail.sg>
2025-04-02 01:51:52 +0800

    Update GetWeatherResult to use DataContext

--- src/components/GetWeatherResult/GetWeatherResult.test.tsx
+++ src/components/GetWeatherResult/GetWeatherResult.test.tsx
@@ -1,98 +1,42 @@
-import { formatDateTime } from '@/lib/formatDateTime';
-import { MOCK_WEATHER_SUCCESS } from '@/lib/mocks';
-import { WeatherFailureModel } from '@/modules/Weather/WeatherModel';
-import { render, screen, waitFor } from '@testing-library/react';
-import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { DataContext } from '@/contexts/Data/DataContext';
+import {
+  MOCK_WEATHER_DISPLAY_FAILURE,
+  MOCK_WEATHER_DISPLAY_SUCCESS,
+} from '@/lib/mocks';
+import { render, screen } from '@testing-library/react';
 import GetWeatherResult from './GetWeatherResult';
 
-jest.mock('../../hooks/useLocalStorage');
-
-const mockFormattedDate = '2024-04-01, 12:00 PM';
-jest.mock('../../lib/formatDateTime', () => ({
-  formatDateTime: jest
-    .fn()
-    .mockReturnValue({ dateTime: '2024-04-01, 12:00 PM' }),
-}));
-
-const mockErrorData = {
-  cod: 404,
-  message: 'City not found',
-} as WeatherFailureModel;
-
 describe('GetWeatherResult', () => {
-  let setSearchHistory: jest.Mock;
-
   beforeEach(() => {
     jest.clearAllMocks();
-    setSearchHistory = jest.fn();
-    (useLocalStorage as jest.Mock).mockReturnValue([[], setSearchHistory]);
   });
 
   it('renders weather information correctly when data is available', () => {
-    render(<GetWeatherResult results={MOCK_WEATHER_SUCCESS} />);
+    render(
+      <DataContext.Provider value={{ weather: MOCK_WEATHER_DISPLAY_SUCCESS }}>
+        <GetWeatherResult />
+      </DataContext.Provider>,
+    );
 
     expect(screen.getByText('London, GB')).toBeInTheDocument();
-
-    expect(screen.getByText('Clouds')).toBeInTheDocument();
+    expect(screen.getByText('Clear')).toBeInTheDocument();
 
     expect(screen.getByText('Description:')).toBeInTheDocument();
-    expect(screen.getByText('broken clouds')).toBeInTheDocument();
+    expect(screen.getByText('clear sky')).toBeInTheDocument();
     expect(screen.getByText('Temperature:')).toBeInTheDocument();
-    expect(screen.getByText('281.24°C ~ 283.72°C')).toBeInTheDocument();
+    expect(screen.getByText('284.82°C ~ 286.47°C')).toBeInTheDocument();
     expect(screen.getByText('Humidity:')).toBeInTheDocument();
-    expect(screen.getByText('80%')).toBeInTheDocument();
+    expect(screen.getByText('56%')).toBeInTheDocument();
     expect(screen.getByText('Time:')).toBeInTheDocument();
-    expect(screen.getByText(mockFormattedDate)).toBeInTheDocument();
-
-    expect(formatDateTime).toHaveBeenCalledWith(MOCK_WEATHER_SUCCESS.dt * 1000);
+    expect(screen.getByText('2025-04-02, 1:16 AM')).toBeInTheDocument();
   });
 
   it('renders error message when no data is found', () => {
-    render(<GetWeatherResult results={mockErrorData} />);
-    expect(screen.getByText('City not found')).toBeInTheDocument();
-  });
-});
-
-describe('GetWeatherResult LocalStorage', () => {
-  let setSearchHistory: jest.Mock;
-
-  beforeEach(() => {
-    setSearchHistory = jest.fn();
-    (useLocalStorage as jest.Mock).mockReturnValue([[], setSearchHistory]);
-  });
-
-  it('should store search history in localStorage when results are successful', async () => {
-    render(<GetWeatherResult results={MOCK_WEATHER_SUCCESS} />);
-
-    await waitFor(() => {
-      expect(setSearchHistory).toHaveBeenCalledWith([
-        { key: 'London, GB', date: mockFormattedDate },
-      ]);
-    });
-  });
-
-  it('should not update search history if the same city is searched again', async () => {
-    (useLocalStorage as jest.Mock).mockReturnValue([
-      [{ key: 'London, GB', date: mockFormattedDate }],
-      setSearchHistory,
-    ]);
-
-    render(<GetWeatherResult results={MOCK_WEATHER_SUCCESS} />);
-
-    await waitFor(() => {
-      expect(setSearchHistory).toHaveBeenCalledWith([
-        { key: 'London, GB', date: mockFormattedDate },
-      ]);
-    });
-  });
-
-  it('should handle failure case and not modify search history', async () => {
-    render(<GetWeatherResult results={mockErrorData} />);
-
-    await waitFor(() => {
-      expect(setSearchHistory).not.toHaveBeenCalled();
-    });
-
+    render(
+      <DataContext.Provider value={{ weather: MOCK_WEATHER_DISPLAY_FAILURE }}>
+        <GetWeatherResult />
+      </DataContext.Provider>,
+    );
     expect(screen.getByText('City not found')).toBeInTheDocument();
   });
 });
--- src/components/GetWeatherResult/GetWeatherResult.tsx
+++ src/components/GetWeatherResult/GetWeatherResult.tsx
@@ -1,87 +1,29 @@
 'use client';
 
 import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
-import { useLocalStorage } from '@/hooks/useLocalStorage';
-import { formatDateTime } from '@/lib/formatDateTime';
-import {
-  WeatherFailureModel,
-  WeatherSuccessModel,
-} from '@/modules/Weather/WeatherModel';
-import React, { useEffect, useState } from 'react';
+import { useDataContext } from '@/contexts/Data/DataContext';
+import React from 'react';
 
-interface SearchHistory {
-  key: string;
-  date: string;
-}
-interface ResultTable {
-  Description: string;
-  Temperature: string;
-  Humidity: string;
-  Time: string;
-}
-interface GetWeatherResultProps {
-  results?: WeatherSuccessModel | WeatherFailureModel | null;
-}
-const GetWeatherResult: React.FC<GetWeatherResultProps> = (props) => {
-  const { results } = props;
-  const [resultTable, setResultTable] = useState<ResultTable | undefined>();
-  const [weatherSuccess, setWeatherSuccess] = useState<
-    WeatherSuccessModel | undefined
-  >();
-  const [weatherFailure, setWeatherFailure] = useState<
-    WeatherFailureModel | undefined
-  >();
-
-  // localStorage states
-  const [searchHistory, setSearchHistory] =
-    useLocalStorage<SearchHistory[]>('searchHistory');
-
-  useEffect(() => {
-    if (results?.cod === 200) {
-      const weatherResults = results as WeatherSuccessModel;
-      const dateTime = formatDateTime(weatherResults.dt * 1000)?.dateTime;
-      setWeatherSuccess(weatherResults);
-      setResultTable({
-        Description: weatherResults.weather[0].description,
-        Temperature: `${weatherResults.main.temp_min}°C ~ ${weatherResults.main.temp_max}°C`,
-        Humidity: `${weatherResults.main.humidity}%`,
-        Time: dateTime,
-      });
-      setWeatherFailure(undefined);
-
-      const search: SearchHistory = {
-        key: `${weatherResults.name}, ${weatherResults.sys.country}`,
-        date: dateTime,
-      };
-      if (searchHistory) {
-        const nextWeatherList = searchHistory.filter(
-          (item) => item.key !== search.key,
-        );
-        setSearchHistory([search, ...nextWeatherList]);
-      } else {
-        setSearchHistory([search]);
-      }
-    } else {
-      setWeatherSuccess(undefined);
-      setResultTable(undefined);
-      setWeatherFailure(results as WeatherFailureModel);
-    }
-  }, [results]);
+const GetWeatherResult: React.FC = () => {
+  const { weather } = useDataContext();
 
   return (
     <div className="mt-7">
-      {weatherSuccess && resultTable && (
+      {weather?.cod === 200 && (
         <div>
-          <p className="text-muted-foreground">
-            {weatherSuccess.name}, {weatherSuccess.sys.country}
-          </p>
+          <p className="text-muted-foreground">{weather.name}</p>
           <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
-            {weatherSuccess.weather[0].main}
+            {weather.main}
           </h1>
 
           <Table className="w-fit min-w-[346px] mt-4">
             <TableBody>
-              {Object.entries(resultTable).map((item) => {
+              {Object.entries({
+                Description: weather.description,
+                Temperature: weather.temperature,
+                Humidity: weather.humidity,
+                Time: weather.time,
+              }).map((item) => {
                 const [key, value] = item;
 
                 return (
@@ -97,9 +39,9 @@ const GetWeatherResult: React.FC<GetWeatherResultProps> = (props) => {
           </Table>
         </div>
       )}
-      {weatherFailure && (
+      {weather && weather.cod !== 200 && (
         <div className="w-full bg-red-300 border-red-800 border-solid border-2 py-1 px-2">
-          {weatherFailure.message}
+          {weather.message}
         </div>
       )}
     </div>
